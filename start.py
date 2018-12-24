0f6af2c5b0fc574d35b2e47ef3df1d8a5a32f30c
Sevak Mardirosian <smardirosians@gmail.com>
2018-12-24 13:35:55 +0100

    adding start

--- start.py
+++ start.py
@@ -1,10 +1,10 @@
 from core.data_aug import *
-from core.bbox_util import *
+from core.bbox_util import draw_rect
 import numpy as np 
 import cv2 
 import matplotlib.pyplot as plt 
 import pickle as pkl
-# https://github.com/Paperspace/DataAugmentationForObjectDetection/blob/master/quick-start.ipynb
+
 # Storage Format
 # First we define how the storage formats required for images to work
 # The image: An open-cv numpy array of shape (HxHxC)
@@ -12,5 +12,63 @@ import pickle as pkl
 # 5 columns represent the top-left x-coordinate, top-left y-coordinate, bottom-right x-coordinate, bottom-right y-coordinate, and the class of the object. 
 
 img = cv2.imread("messi.jpg")[:,:,::-1] # opencv loads images in bgr. 
-bboxes = pkt.load(open("messi_aan.pkl", "rb"))
-print(bboxes)
+bboxes = pkl.load(open("messi_ann.pkl", "rb"))
+# print(bboxes)
+
+plotte_img = draw_rect(img, bboxes)
+plt.imshow(plotte_img)
+
+plt.show()
+
+
+#Horizontal Flipping.
+img_, bboxes_ = RandomHorizontalFlip(1)(img.copy(), bboxes.copy())
+plotted_img = draw_rect(img_, bboxes_)
+plt.imshow(plotted_img)
+plt.show()
+
+#Scaling
+img_, bboxes_ = RandomScale(0.3, diff = True)(img.copy(), bboxes.copy())
+plotted_img = draw_rect(img_, bboxes_)
+plt.imshow(plotted_img)
+plt.show()
+
+
+#Translation
+img_, bboxes_ = RandomTranslate(0.3, diff = True)(img.copy(), bboxes.copy())
+plotted_img = draw_rect(img_, bboxes_)
+plt.imshow(plotted_img)
+plt.show()
+
+#Rotation
+img_, bboxes_ = RandomRotate(20)(img.copy(), bboxes.copy())
+plotted_img = draw_rect(img_, bboxes_)
+plt.imshow(plotted_img)
+plt.show()
+
+
+#Shearing
+img_, bboxes_ = RandomShear(0.2)(img.copy(), bboxes.copy())
+plotted_img = draw_rect(img_, bboxes_)
+plt.imshow(plotted_img)
+plt.show()
+
+# Resizing
+img_, bboxes_ = Resize(608)(img.copy(), bboxes.copy())
+plotted_img = draw_rect(img_, bboxes_)
+plt.imshow(plotted_img)
+plt.show()
+
+# HSV transforms are supported as well.
+img_, bboxes_ = RandomHSV(100, 100, 100)(img.copy(), bboxes.copy())
+plotted_img = draw_rect(img_, bboxes_)
+plt.imshow(plotted_img)
+plt.show()
+
+# You can combine multiple transforms together by using the Sequence class as follows.
+seq = Sequence([RandomHSV(40, 40, 30),RandomHorizontalFlip(), RandomScale(), RandomTranslate(), RandomRotate(10), RandomShear()])
+img_, bboxes_ = seq(img.copy(), bboxes.copy())
+
+plotted_img = draw_rect(img_, bboxes_)
+plt.imshow(plotted_img)
+plt.show()
--- test.py
+++ test.py
@@ -6,13 +6,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# img = cv2.imread("messi.jpg")[:,:,::-1] #OpenCV uses BGR channels
-# bboxes = pkl.load(open("messi_ann.pkl", "rb"))
+img = cv2.imread("messi.jpg")[:,:,::-1] #OpenCV uses BGR channels
+bboxes = pkl.load(open("messi_ann.pkl", "rb"))
 
 
-# transforms = Sequence([RandomHorizontalFlip(1), RandomScale(0.2, diff = True), RandomRotate(10)])
+transforms = Sequence([RandomHorizontalFlip(1), RandomScale(0.2, diff = True), RandomRotate(10)])
 
-# img, bboxes = transforms(img, bboxes)
+img, bboxes = transforms(img, bboxes)
 
-# plt.imshow(draw_rect(img, bboxes))
+plt.imshow(draw_rect(img, bboxes))
 
